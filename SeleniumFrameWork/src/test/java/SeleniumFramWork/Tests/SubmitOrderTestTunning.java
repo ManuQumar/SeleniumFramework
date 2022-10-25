ef1d7e845b22bebe6bb8e77e828f7134bb13eb70
Manohar <Manohar@MSI>
2022-10-26 00:53:37 +0530

    Implemented Parameterization~~

--- SeleniumFrameWork/src/test/java/SeleniumFramWork/Tests/SubmitOrderTestTunning.java
+++ SeleniumFrameWork/src/test/java/SeleniumFramWork/Tests/SubmitOrderTestTunning.java
@@ -1,10 +1,12 @@
 package SeleniumFramWork.Tests;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import SeleniumFramWork.PageObjects.CartPageObjects;
@@ -16,21 +18,24 @@ import SeleniumFramWork.PageObjects.ProductCataloguePageObjects;
 import SeleniumFrameWork.TestComponents.BaseTest;
 
 public class SubmitOrderTestTunning extends BaseTest{
-	String ProductName="ZARA COAT 3";
-	@Test
-	public void SubmitOrder() throws IOException, InterruptedException {
-		
+//	String ProductName="ZARA COAT 3";
+	@Test(dataProvider= "getData" , groups={"purchaseOrder"})
+	// passing the strings and grouping to run this test Individually
+//	public void SubmitOrder(String EmailId , String Password, String ProductName) throws IOException, InterruptedException {
+	public void SubmitOrder(HashMap<String , String> input) throws IOException, InterruptedException {
+
+	// here we drive the parameters using HashMap so commenting the above line	
 		// Here I know we should hit the Login Page so I declared it in BaseTest LauchApplication method
 		// as we extending the BaseTest we can have the access of Parent variables 
 		//As we using testNG method with the help of @BeforeTest annotation we can comment the below line
 //	LandingPageObjects LP=	LaunchApplication();
-	LP.Loginapp("manum7792@gmail.com", "Rahul@123");
+	LP.Loginapp(input.get("EmailId"),input.get("Password"));
 	ProductCataloguePageObjects Prod= new ProductCataloguePageObjects(driver);
 		List<WebElement> products=Prod.getProductsList();
-	Prod.addProductToCart(ProductName);
+	Prod.addProductToCart(input.get("ProductName"));
 	Prod.goToCartPage();
 	CartPageObjects CartPage= new CartPageObjects(driver);
- Boolean match=	CartPage.VerifyProductDisplay(ProductName);
+ Boolean match=	CartPage.VerifyProductDisplay(input.get("ProductName"));
     Assert.assertTrue(match);
 	CartPage.CheckOut();
 	CheckoutPageObjects CP= new CheckoutPageObjects(driver);
@@ -41,7 +46,7 @@ public class SubmitOrderTestTunning extends BaseTest{
 	System.out.println(confirmMessage);
 }
 	@Test(dependsOnMethods={"SubmitOrder"})
-	public void OrderHistory() {
+	public void OrderHistory(String ProductName) {
 		LP.Loginapp("manum7792@gmail.com", "Rahul@123");
 		ProductCataloguePageObjects Prod= new ProductCataloguePageObjects(driver);
 		Prod.goToOrderPage();
@@ -49,4 +54,19 @@ public class SubmitOrderTestTunning extends BaseTest{
 	Assert.assertTrue(OrdersPage.VerifyOrderDisplay(ProductName));
 	System.out.println("Done!");
 	}
+	// here Implementing testNG data Provider to run the test with mutiple set of Data
+	@DataProvider
+	public Object[][] getData() {
+		HashMap<String, String> HMap= new HashMap<String, String>();
+		HMap.put("EmailId", "manum7792@gmail.com");
+		HMap.put("Password", "Rahul@123");
+		HMap.putIfAbsent("ProductName", "ZARA COAT 3");
+		HashMap<String,String> HMap1 = new HashMap<String,String>();
+		HMap1.put("EmailId", "paul@gmail.com");
+		HMap1.put("Password", "Paul@321");
+		HMap1.put("ProductName", "ADIDAS ORIGINAL");
+		return	new Object[][] { {HMap }, {HMap1}};
+	//	return	new String[][] { {"manum7792@gmail.com" ,"Rahul@123","ZARA COAT 3" }, {"paul@gmail.com","Paul@321","ADIDAS ORIGINAL"}};
+	}
+	
 }
